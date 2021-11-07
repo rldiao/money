856d0f19fb8f7164d4cf25d63867565322401747
Robert Diao <43899833+rldiao@users.noreply.github.com>
2021-11-07 15:18:39 +1100

    update DB_URL to money.db

--- money/db/session.py
+++ money/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "sqlite:///db.sqlite3"
+DB_URL = "sqlite:///money.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 
